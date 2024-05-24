c2443606978d19ee416eecce21dae0d6b384c57c
Lincoln Nguyen <137611486+rinkaaan@users.noreply.github.com>
2024-05-24 14:58:28 -0700

    Add Mailgun

---
+++ src/main/java/com/rikagu/accounts/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.rikagu.accounts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class AppConfig {
+
+    private final Environment  environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public net.sargue.mailgun.Configuration configuration() {
+        return new net.sargue.mailgun.Configuration()
+                .domain("rinkagu.com")
+                .apiKey(environment.getProperty("mailgun.api-key"))
+                .from("Rinkagu", "noreply@rinkagu.com");
+    }
+}
--- src/main/java/com/rikagu/accounts/controller/HelloController.java
+++ src/main/java/com/rikagu/accounts/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.rikagu.accounts.controller;
 
+import net.sargue.mailgun.Configuration;
+import net.sargue.mailgun.Mail;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class HelloController {
 
+    private final Configuration configuration;
+
+    public HelloController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/hello")
     public String hello() {
+        Mail.using(configuration)
+                .to("test@email.com")
+                .subject("This is the subject")
+                .text("Hello world!")
+                .build()
+                .send();
+
         return "Hello, World!";
     }
 }
